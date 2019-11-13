22a6d19971d4b80781b87bb1c0dcf68447a6b9bc
Abdullah Ayyad <ab.ayyad@gmail.com>
2019-11-13 14:01:47 +0200

    add new search view controller

---
+++ d/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  d
+//
+//  Created by Abdullah Ayyad on 13/11/2019.
+//  Copyright © 2019 Abdullah Ayyad. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

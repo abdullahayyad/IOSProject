f64566c246a70b8155e8affa1c126270c427df15
Abdullah Ayyad <ab.ayyad@gmail.com>
2019-11-13 13:58:58 +0200

    add x variable

--- d/ViewController.swift
+++ d/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var x  = 5
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
